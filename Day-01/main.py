1e922d631db5329de23ae657906c460261244508
Shreycode27 <yshrey127@gmail.com>
2026-03-09 13:39:03 +0530

    Day 1 : Fixed If-Else logic for weird/not weird challenge

--- Day-01/main.py
+++ Day-01/main.py
@@ -1,4 +1,22 @@
-# Jarvis 2.0 -Day 1: The Greeting System 
-print("System Initializing ...")
-user_name = input("Enter your name , Boss: ")
-print("Hello " +user_name + "!JARVIS 2.0 is now online.")
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    print("Program Started. Type '0' to stop.")
+    while True:
+        n_input = input("Enter the number: ").strip()
+        n=int(n_input)
+        if n == 0:
+            print("shutting down")
+            break
+        if n % 2 != 0:
+            print("Weird")
+        elif n >=2 and n <=5:
+            print("Not Weird")
+        elif n >=6 and n <=20:
+            print("Weird")
+        elif n >20:
+            print("Not Weird")
